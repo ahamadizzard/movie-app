da610b56111e14707f04ff8c79619a4bf501a721
ahamadizzard <ahamadizzard@hotmail.com>
2025-11-01 13:50:33 +0530

    theme config

---
+++ app/login/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Login() {
+    return (
+        <div className="w-full min-h-screen bg-yellow-200 text-red-500 flex items-center justify-center">
+            <div className="bg-green-300 p-2 rounded-xl">Login Page</div>
+        </div>
+    );
+}
